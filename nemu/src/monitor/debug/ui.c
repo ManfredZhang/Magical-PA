8de485e3874c2faa4e6b414075c1a91f2700c5b4
tracer-ics2017 <tracer@njuics.org>
2017-09-14 15:00:35 +0800

    >  compile 161220169 Manfred Linux 6830aa7d021b 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  15:00:35 up 22:17,  4 users,  load average: 0.00, 0.00, 0.00 d709eb9d674694c2bd1c8ace67195f1d582be27a

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -83,7 +83,8 @@ static int cmd_x(char *args)
 	int xdir_int = XtoD(atoi(xdir));
 	uint32_t xdir_u = xdir_int;
 	for (int i = 0; i < xnum_int; i++)
-		printf("0x%X\n",vaddr_read(xdir_u+i, 1));
+		printf("0x%X, ",vaddr_read(xdir_u+i, 1));
+	printf("\n");
 	
 	return 0;
 }
