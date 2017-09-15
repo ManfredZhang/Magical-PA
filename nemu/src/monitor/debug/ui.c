6c093c6d55db8d2e986c9a9c4eebc4964e7efa8e
tracer-ics2017 <tracer@njuics.org>
2017-09-15 17:33:38 +0800

    >  compile 161220169 Manfred Linux 6830aa7d021b 4.9.41-moby #1 SMP Wed Sep 6 00:05:16 UTC 2017 x86_64 GNU/Linux  17:33:38 up 1 day, 15:34,  4 users,  load average: 0.00, 0.01, 0.00 8fff3d54d43331b3910639181f501a4b48bc3c7c

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -78,18 +78,19 @@ static int cmd_x(char *args)
 {
 	char *xnum = strtok(args, " ");
 	char *xdir = xnum + strlen(xnum) + 3;
-	//int xnum_int = atoi(xnum);
+	int xnum_int = atoi(xnum);
 
 	int xdir_int = XtoD(atoi(xdir));
 	uint32_t xdir_u = xdir_int;
-	//for (int i = 0; i < xnum_int-1; i++)
-	//	printf("0x%X, ",vaddr_read(xdir_u+4*i, 4));
-	//printf("0x%X\n",vaddr_read(xdir_u+ xnum_int - 4, 4));
-	printf("%X, ",vaddr_read(xdir_u, 4));
-	printf("%X, ",vaddr_read(xdir_u, 3));
-	printf("%X, ",vaddr_read(xdir_u, 2));
-	printf("%X, ",vaddr_read(xdir_u, 1));
-	printf("%X, ",vaddr_read(xdir_u, 0));
+	for (int i = 0; i < 4*(xnum_int); i++)
+	{
+		if (i % 4 == 0)
+			printf("0x");
+		printf("%X",vaddr_read(xdir_u+i, 1));
+		if (i % 4 == 0)
+			printf("\n");
+	}
+	//printf("0x%X\n",vaddr_read(xdir_u+ xnum_int - , 1));
 
 	return 0;
 }
