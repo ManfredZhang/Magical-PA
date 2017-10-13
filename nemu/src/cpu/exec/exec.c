648691f3054b63f370f8a72c3f5a4e445c3c038f
tracer-ics2017 <tracer@njuics.org>
2017-10-13 08:14:38 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  08:14:38 up 1 day, 12:56,  7 users,  load average: 0.00, 0.00, 0.00 a577551deb6cae8fa7d177c090a76dd7b850da81

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -84,7 +84,7 @@ opcode_entry opcode_table [512] = {
   /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x28 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x30 */	EMPTY, IDEXW(SI2E, xor, 1), EMPTY, EMPTY,
+  /* 0x30 */	EMPTY, IDEX(r, xor), EMPTY, EMPTY,
   /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x38 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x3c */	EMPTY, EMPTY, EMPTY, EMPTY,
