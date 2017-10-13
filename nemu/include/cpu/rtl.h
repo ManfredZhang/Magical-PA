d26e5470b58e778f2be2b170cc2b90da12a880b3
tracer-ics2017 <tracer@njuics.org>
2017-10-13 06:56:26 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  06:56:26 up 1 day, 11:43,  7 users,  load average: 0.08, 0.02, 0.01 fd2ddf4e246ecf242bded185336f1f7594bb1fd

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -113,10 +113,10 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1) {
 
 #define make_rtl_setget_eflags(f) \
   static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
-	TODO();\
+	cpu.flags.f = *src;\
   } \
   static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
-	TODO();\
+	*dest = cpu.flags.f;\
   }
 
 make_rtl_setget_eflags(CF)
@@ -178,12 +178,12 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  TODO();
+  //TODO();
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
