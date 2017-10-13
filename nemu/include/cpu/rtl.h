9a43ee9a6d2e0445b10375506ec2c8c2e028ecae
tracer-ics2017 <tracer@njuics.org>
2017-10-13 07:13:01 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  07:13:01 up 1 day, 11:59,  7 users,  load average: 0.05, 0.02, 0.00 e373284ffc60bbeed5530f2f4a46b76967fad9f2

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -178,12 +178,12 @@ static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-  //TODO();
+  TODO();
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-  //TODO();
+  TODO();
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
