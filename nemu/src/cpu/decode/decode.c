5be1e356e088e041279e6e0f818374d49102c11d
tracer-ics2017 <tracer@njuics.org>
2017-10-19 07:19:44 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  07:19:44 up  4:56,  4 users,  load average: 0.05, 0.04, 0.01 422167028c9f4c59550af951fbafe357dbff2004

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -42,7 +42,7 @@ static inline make_DopHelper(SI) {
   uint32_t a = instr_fetch(eip, op->width);
   if (a >> 31 == 1)
 	  a += 0xffff0000;
-  printf("zzmf: %u\n",a);
+  printf("zzmf: %u\n",op->width);
   op->simm = a;
 
   rtl_li(&op->val, op->simm);
