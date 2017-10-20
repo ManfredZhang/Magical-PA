17b46170471c036cbe94b1b7e018528ec6df9d98
Manfred Zhang <zmf2468@163.com>
2017-10-20 22:53:23 +0800

    reg！

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -28,9 +28,55 @@ typedef struct {
   /* In NEMU, rtlreg_t is exactly uint32_t. This makes RTL instructions
    * in PA2 able to directly access these registers.
    */
-	struct {
-		rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
-	 };
+	struct{
+		union{
+			uint32_t eax;
+			uint16_t ax;
+			struct{
+				uint8_t al;
+				uint8_t ah;
+			};
+		};
+		union{
+			uint32_t ecx;
+			uint16_t cx;
+			struct{
+				uint8_t cl;
+				uint8_t ch;
+			};
+		};
+		union{
+			uint32_t edx;
+			uint16_t dx;
+			struct{
+				uint8_t dl;
+				uint8_t dh;
+			};
+		};	
+		union{
+			uint32_t ebx;
+			uint16_t bx;
+			struct{
+				uint8_t bl;
+				uint8_t bh;
+			};
+		};			
+		union{
+			uint32_t esp;
+			uint16_t sp;
+		};
+		union{
+			uint32_t ebp;
+			uint16_t bp;
+		};
+		union{
+			uint32_t esi;
+			uint16_t si;
+		};			
+		union{
+			uint32_t edi;
+			uint16_t di;
+		};		
 
   };
 
