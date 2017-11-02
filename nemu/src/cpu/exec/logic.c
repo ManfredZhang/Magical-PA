7dcef524b7e910b97a40a4db5c3a08c4f0b48ca9
tracer-ics2017 <tracer@njuics.org>
2017-11-02 15:31:19 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  15:31:19 up  8:05,  6 users,  load average: 0.08, 0.09, 0.11 60302d8d8ccf040b60ba1940473bc5c6eb3f79c4

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -48,6 +48,15 @@ make_EHelper(or) {
 make_EHelper(sar) {
   //TODO();
   // unnecessary to update CF and OF in NEMU
+  /*rtl_sar(&t2, &id_dest->val, &id_src->val);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);*/
+
+  if (id_dest->width == 1)
+	  id_dest->val = (int32_t)((int8_t)id_dest->val);
+  else if(id_dest->width == 2)
+	  id_dest->val = (int32_t)((int16_t) id_dest->val);
+     
   rtl_sar(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
   rtl_update_ZFSF(&t2, id_dest->width);
