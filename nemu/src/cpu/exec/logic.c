89dd56e2aa4bee2c92093a0b5d74958f4a4cb991
tracer-ics2017 <tracer@njuics.org>
2017-11-04 04:29:16 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  04:29:16 up 21:29,  3 users,  load average: 0.19, 0.38, 0.28 fb71f890f843085c6524c48462d4cab8bb4088dd

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -61,9 +61,9 @@ make_EHelper(sar) {
   // unnecessary to update CF and OF in NEMU
 
   if (id_dest->width == 1)
-	  id_dest->val = (int32_t)((int8_t)id_dest->val);
+	  id_dest->val = (int8_t)id_dest->val;
   else if(id_dest->width == 2)
-	  id_dest->val = (int32_t)((int16_t) id_dest->val);
+	  id_dest->val = (int16_t)id_dest->val;
      
   rtl_sar(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
