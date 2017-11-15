8d4b7edb9d51971cc1488eca9fe4568b95f8fcd0
tracer-ics2017 <tracer@njuics.org>
2017-11-15 05:20:56 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  05:20:56 up  6:31,  3 users,  load average: 0.03, 0.08, 0.04 b7d920779abafcbf8692bde48ecc04fb27c36b92

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,7 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
   //TODO();
-  rtl_push(&cpu.EFLAGS);
+  /*rtl_push(&cpu.EFLAGS);
   t0 = cpu.cs;
   rtl_push(&t0);
   rtl_push(&ret_addr);
@@ -15,7 +15,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   uint32_t offset_low = vaddr_read(cpu.idtr.base + NO*8 + 4, 4) & 0x0000ffff;
 
   decoding.is_jmp = 1;
-  decoding.jmp_eip = offset_high | offset_low;
+  decoding.jmp_eip = offset_high | offset_low;*/
+	rtl_push(&cpu.EFLAGS);
+		t0 = cpu.cs;
+			rtl_push(&t0);;
+				rtl_push(&ret_addr);
+					uint32_t target_eip_low, target_eip_high;
+						target_eip_low = vaddr_read(cpu.idtr.base + NO * 8, 4) & 0x0000ffff;
+							target_eip_high = vaddr_read(cpu.idtr.base + NO * 8 + 4, 4) & 0xffff0000;
+							   decoding.is_jmp = 1;   
+							      decoding.jmp_eip = target_eip_low | target_eip_high;
 }
 
 void dev_raise_intr() {
