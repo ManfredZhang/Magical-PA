656f25bb733db8dedd47affce18087861b54fb4b
tracer-ics2017 <tracer@njuics.org>
2017-11-15 05:46:10 +0000

    >  compile 161220169 Manfred Linux 0cc3ae467ed7 4.9.49-moby #1 SMP Wed Sep 27 23:17:17 UTC 2017 x86_64 GNU/Linux  05:46:10 up  6:51,  3 users,  load average: 0.00, 0.00, 0.00 165b98293ae0f7ab5f8651cb25c0f962781fb8de

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -9,11 +9,10 @@ make_EHelper(lidt) {
   rtl_lm(&t0, &id_dest->addr, 2);
   cpu.idtr.limit = t0;
   
-  int temp = 4;
   if (decoding.is_operand_size_16)
-	  temp = 3;
-  rtl_lm(&t0, &id_dest->addr+6-temp, temp);
-  cpu.idtr.base = t0;
+	  rtl_lm(&(cpu.idtr.base), &id_dest->addr+3, 3);
+  else
+	  rtl_lm(&(cpu.idtr.base), &id_dest->addr+2, 4);
 
   /*t1 = id_dest->addr + 2;
   rtl_lm(&t0, &t1, 4);
